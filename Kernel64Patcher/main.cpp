4d61f44c02eadabf1b9b896ac5061347b402ac8a
Cememers <santa150@outlook.com>
2020-08-23 17:14:47 +0900

    Add jailbreak patch

--- Kernel64Patcher/main.cpp
+++ Kernel64Patcher/main.cpp
@@ -11,39 +11,94 @@ using namespace tihmstar::offsetfinder64;
 
 int main(int argc, const char * argv[]) {
     FILE* fp = NULL;
-    bool get_AMFI_patch = false;
-    int flags = 0;
     
-    if(argc < 4) {
-        printf("Usage: %s <kernel_in> <kernel_out> [args]\n", argv[0]);
-        printf("\t-a\t\tDisable AMFI from kernel patch\n");
+    if(argc < 2) {
+        printf("Usage: %s <kernel_in> <kernel_out>\n");
         return -1;
     }
     
     printf("%s: Starting...\n", __FUNCTION__);
     
-    for(int i = 0; i < argc; i++) {
-        
-        if(HAS_ARG("-a", 0)) {
-            get_AMFI_patch = true;
-        }
-    }
-    
-    if (!get_AMFI_patch) {
-        printf("%s: Nothing to patch!\n", __FUNCTION__);
-        return -1;
-    }
-    
     std::vector<patch> patches;
     kernelpatchfinder64 kpf(argv[1]);
-    
-    if (get_AMFI_patch) {
-        printf("Kernel: Adding AMFI_get_out_of_my_way patch...\n");
-        auto p = kpf.get_AMFI_10_11_12_13_patch();
-        patches.insert(patches.end(), p.begin(), p.end());
+
+    printf("Kernel: Adding AMFI patch...\n");
+    auto p = kpf.get_amfi_patch();
+    patches.insert(patches.end(), p.begin(), p.end());
+
+    /* Jailbreak patch by liboffsetfinder64 */
+    try {
+        printf("Kernel: Adding MarijuanARM patch...\n");
+        auto pp = kpf.get_MarijuanARM_patch();
+        patches.insert(patches.end(), pp.begin(), pp.end());
     }
-    
-    
+    catch(...){
+        printf("Failure:MarijuanARM patch\n");
+    }
+
+    try {
+    printf("Kernel: Adding task_conversion_eval patch...\n");
+    auto ppp = kpf.get_task_conversion_eval_patch();
+    patches.insert(patches.end(), ppp.begin(), ppp.end());
+    }
+    catch(...){
+        printf("Failure:task_conversion_eval patch\n");
+    }
+
+    try {
+    printf("Kernel: Adding vm_fault_internal patch...\n");
+    auto pppp = kpf.get_vm_fault_internal_patch();
+    patches.insert(patches.end(), pppp.begin(), pppp.end());
+    }
+    catch(...){
+        printf("Failure:vm_fault_internal patch\n");
+    }
+
+    try {
+    printf("Kernel: Adding trustcache_true patch...\n");
+    auto ppppp = kpf.get_trustcache_true_patch();
+    patches.insert(patches.end(), ppppp.begin(), ppppp.end());
+    }
+    catch(...){
+        printf("Failure:trustcache_true patch\n");
+    }
+
+    try {
+    printf("Kernel: Adding mount patch...\n");
+    auto pppppp = kpf.get_mount_patch();
+    patches.insert(patches.end(), pppppp.begin(), pppppp.end());
+    }
+    catch(...){
+        printf("Failure:mount patch\n");
+    }
+
+    try {
+    printf("Kernel: Adding tfp0 patch...\n");
+    auto ppppppp = kpf.get_tfp0_patch();
+    patches.insert(patches.end(), ppppppp.begin(), ppppppp.end());
+    }
+    catch(...){
+        printf("Failure:tfp0 patch\n");
+    }
+
+    try{
+    printf("Kernel: Adding get_task_allow patch...\n");
+    auto pppppppp = kpf.get_get_task_allow_patch();
+    patches.insert(patches.end(), pppppppp.begin(), pppppppp.end());
+    }
+    catch(...){
+        printf("Failure:get_task_allow patch\n");
+    }
+
+    try { 
+    printf("Kernel: Adding apfs_snapshot patch...\n");
+    auto ppppppppp = kpf.get_apfs_snapshot_patch();
+    patches.insert(patches.end(), ppppppppp.begin(), ppppppppp.end());
+    }
+    catch(...){
+        printf("Failure:apfs_snapshot patch\n");
+    }
+
     /* Write out the patched file... */
     fp = fopen(argv[2], "wb+");
     if(!fp) {
